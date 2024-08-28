77228dff35d70428bf85b57c3ac53fe20e6de820
Henry Le <151426288+LeTanQuocc@users.noreply.github.com>
2024-08-28 22:38:05 +0700

    swapping.java

--- src/arrays/swapping.java
+++ src/arrays/swapping.java
@@ -6,7 +6,7 @@ public class swapping {
     private static int i;
     public static void main(String[] args) {
         int [] numbers = {11,42,-5,27,0,89};
-        for(int i = 0; i<numbers.length/2;i++);
+        for(int i = 0; i<numbers.length/2;i++)
         {
             int temp = numbers[i];
             numbers[i]=numbers[numbers.length-1-i];
